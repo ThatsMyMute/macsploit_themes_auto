e781d83cd61b8b6160f6f28d61dc432c84c918c5
ThatsMyMute <75107278+ThatsMyMute@users.noreply.github.com>
2023-11-21 06:46:09 -0500

    Create 1-install.sh

---
+++ 1-install.sh
@@ -0,0 +1,11 @@
+main() {
+    cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
+    echo -e "Dox Bin Theme intalling.."
+    echo -e "Dox Bin theme 90% done.."
+    echo -e "receiving xferd.."
+    curl "https://raw.githubusercontent.com/ThatsMyMute/macsploit_themes_auto/main/1.js" -o "./theme-tomorrow_night.js"
+    echo -e "automatic theme install by ThatsMyMute Credit to forensnoway for making the script!"
+    echo -e "DM ME FOR YOUR THEMES!"
+}
+
+main

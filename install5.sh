625cdf2e71fd2ba307760c31c618dc5466df6cd1
ThatsMyMute <75107278+ThatsMyMute@users.noreply.github.com>
2023-11-21 18:25:07 -0500

    Create install5.sh

---
+++ install5.sh
@@ -0,0 +1,11 @@
+main() {
+    cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
+    echo -e "THEME INSTALL FUCK YOU IM LAZY TO NAME SHIT"
+    echo -e "Feel free to massage by pickle for money"
+    echo -e "receiving xferd.."
+    curl "https://raw.githubusercontent.com/ThatsMyMute/macsploit_themes_auto/main/5.js" -o "./theme-tomorrow_night.js"
+    echo -e "automatic theme install by ThatsMyMute Credit to forensnoway for making the script!"
+    echo -e "DM ME FOR YOUR THEMES!"
+}
+
+main

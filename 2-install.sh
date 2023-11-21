9faa7e248b3bdd3ee50773522c4a8aa5c419902b
ThatsMyMute <75107278+ThatsMyMute@users.noreply.github.com>
2023-11-21 06:51:50 -0500

    Create 2-install.sh

---
+++ 2-install.sh
@@ -0,0 +1,11 @@
+main() {
+    cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
+    echo -e "MY MELODY Theme intalling.."
+    echo -e "MYMELODY Theme 90% done.."
+    echo -e "receiving xferd.."
+    curl "https://raw.githubusercontent.com/ThatsMyMute/macsploit_themes_auto/main/2.js" -o "./theme-tomorrow_night.js"
+    echo -e "automatic theme install by ThatsMyMute Credit to forensnoway for making the script!"
+    echo -e "DM ME FOR YOUR THEMES!"
+}
+
+main
